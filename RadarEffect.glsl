2a2696dd6d5cac42b3ccfeb6639ae4a2add007f3
yuriy.peysakhov <yuriy.peysakhov@gmail.com>
2020-05-25 13:02:49 -0400

    improvements for radar effect

--- RadarEffect.glsl
+++ RadarEffect.glsl
@@ -1,65 +1,45 @@
 #define n 10.
 
-vec2 rand(vec2 p) {
-    return vec2(fract(sin(dot(sin(p), vec2(12.9898, 78.233))) * 143758.5453),
-                fract(sin(dot(sin(p.yx), vec2(12.9898, 78.233))) * 143758.5453));
-}
-
-vec2 enemy = vec2(-0.2);
+#define rand(p) vec2(fract(sin(sin(p) * mat2(12.9898, 78.233, 78.233, 12.9898)) * 143758.5453))
+#define Circle(w, p, r) smoothstep( w, 0., abs(length(p) - r ) )
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    vec2 uv = (fragCoord - 0.5 * iResolution.x) / iResolution.y + vec2(0., 0.4);
+    vec2 R = iResolution.xy;
+    vec2 uv = (fragCoord - 0.5 * R) / R.y;
     float t = iTime;
     vec3 col = vec3(0.);
     vec2 p = uv;
-    vec2 p0 = vec2(0., 0.);
     vec2 p1 = vec2(sin(t), cos(t));
-    vec2 p01 = p1 - p0;
-    vec2 nP01 = normalize(p01);    
-    vec2 p0p = p - p0;
-    vec2 nP0P = normalize(p0p);
-    vec2 p1p = p - p1;
-    vec2 nP1P = normalize(p1p);    
-    float projP0P = dot(p0p, nP01);
-    vec2 projP0PV = projP0P * nP01;
-    vec2 PtoP0P1 = p0p - projP0PV;
+    vec2 PtoP0P1 = p - dot(p1, p) * p1 / dot(p1, p1);
     float d = length(PtoP0P1);
-    if (dot(nP0P, nP01) < 0.) {
-        d = length(p0p);
+    if (dot(p, p1) < 0.) {
+        d = length(p);
     }
-    if (dot(nP1P, -nP01) < 0.) {
-        d = length(p1p);
+    if (dot(p - p1, -p1) < 0.) {
+        d = length(p - p1);
     }
-    vec3 cl = vec3(0.5, 0., 0.);
+    vec2 cl = vec2(0.5, 0.);
     col = .006 / d * cl.yxy;
-    float r = length(p01);
+    float r = length(p1);
+    float i = round(n * length(p) / r);
+    float w = 2. * r / R.y;
+    float circle = Circle(w, p, r * i / n);
     
-    for (float i = 0.; i <= n; i++) {
-        float circle = smoothstep(0.0035, 0., abs(length(p) - mix(0., r, i / n)));
+    col += circle * cl.yxy;
+    
+    vec2 delay = vec2(1., 20.) * (1. + p1); 
+    if (delay.x > 0.99) {
+        float echoR = mix(r, 0., delay.y);
+        float circle = Circle(w, p, echoR);
         col += circle * cl.yxy;
+    } else {
+        vec2 enemy = vec2(-0.2 + 0.5 * cos(0.01 * floor(t)));
+        col += .005 / length(p - enemy) * cl.xyy;
     }
+        
+    float dTargets = length(rand(p - 1e-6 * log(t + 100.)));    
+    col += .02 / dTargets * cl.yxy;
     
-    float delayS = 2. * (0.5 + 0.5 * sin(t));
-    float delayC = 40. * (0.5 + 0.5 * cos(t));
-    
-    enemy += 0.5 * cos(0.01 * floor(t));
-    float dEnemy = .005 / length(p - enemy);
-    vec3 enemyCol = dEnemy * cl.xyy;     
-    
-    if (delayS > 0.99) {
-        float echoR = mix(r, 0., delayC);
-        float circle = smoothstep(0.004, 0., abs(length(p) - echoR));
-        col += circle * cl.yxy;
-        enemyCol = vec3(0.);
-    }
-   
-    col += enemyCol;
-    
-    vec2 targets = rand(p - pow(10., -6.) * log(t + 100.));
-    float dTargets = length(targets);
-    vec3 targetCol = .02 / dTargets * cl.yxy;
-    col += targetCol;
-    
-    fragColor = vec4(col, 1.0);
+    fragColor = vec4(col, 0.0);
 }
